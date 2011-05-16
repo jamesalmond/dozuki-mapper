7ac451d0720e4ad8c4f8ae6114e0940035625cf7
JamesAlmond <james@jamesalmond.com>
2011-05-16 14:36:08 +0100

    Implemented new DSL class style

--- lib/dozuki-mapper.rb
+++ lib/dozuki-mapper.rb
@@ -2,8 +2,24 @@ require 'dozuki'
 require 'dozuki-mapper/proxy'
 module Dozuki
   module Mapper
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
     def map_from(node)
-      yield Proxy.new(self, node)
+      self.class.mapper.call Proxy.new(self, node)
+    end
+    module ClassMethods
+      def map_with(&blk)
+        @mapper = blk
+      end
+      def mapper
+        @mapper
+      end
+      def from_node(node)
+        new.tap do |instance|
+          instance.map_from(node)
+        end
+      end
     end
   end
 end
--- spec/dozuki-mapper_spec.rb
+++ spec/dozuki-mapper_spec.rb
@@ -4,22 +4,47 @@ module Dozuki
   describe Mapper do
     let(:node) { mock :node }
     let(:klass){ Class.new }
-    let(:instance) { klass.new }
-    let(:proxy) { mock :proxy }
-    before { Mapper::Proxy.stub(:new).and_return(proxy) }
-    subject {
+    before do
       klass.send(:include, Mapper)
-      instance.map_from(node) do |arg|
-        @called_with = arg
-      end
-    }
-    it "should create a new proxy with the node and the mapper" do
-      Mapper::Proxy.should_receive(:new).with(instance, node).and_return(proxy)
-      subject
     end
-    it "should yield the proxy to the method call" do
-      subject
-      @called_with.should == proxy
+    describe "mappings" do
+      let(:instance) { klass.new }
+      let(:proxy) { mock :proxy }
+      before { Mapper::Proxy.stub(:new).and_return(proxy) }
+      subject {
+        klass.map_with do |arg|
+          @called_with = arg
+        end
+        instance.map_from(node)
+      }
+      it "should create a new proxy with the node and the mapper" do
+        Mapper::Proxy.should_receive(:new).with(instance, node).and_return(proxy)
+        subject
+      end
+      it "should yield the proxy to the block mapped block" do
+        subject
+        @called_with.should == proxy
+      end
+    end
+    describe "from_node" do
+      let(:node) { mock :node }
+      let(:instance) { mock :klass }
+      before do
+        klass.stub(:new).and_return(instance)
+        instance.stub(:map_from)
+      end
+      subject { klass.from_node(node) }
+      it "should create a new instance of the class" do
+        klass.should_receive(:new).and_return(instance)
+        subject
+      end
+      it "should map the instance from the node" do
+        instance.should_receive(:map_from).with(node)
+        subject
+      end
+      it "should return the instance of the class" do
+        subject.should == instance
+      end
     end
   end
 end
